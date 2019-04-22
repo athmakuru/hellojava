5865d339cfad2be61469f217c8b4150caf395977
ybmadhu <ybmadhu707@gmail.com>
2019-04-22 08:38:04 +0530

    added for poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hi this is  pollscm");
-    System.out.println("Hi dvs java");
+    System.out.println("Hi dvs java webhook");
   }
 }
